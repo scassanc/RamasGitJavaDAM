bd85eb42f286fcd6369cd30a2d3cd4ade7d179ac
scassanc <scassanc@g.educaand.es>
2025-11-13 10:55:14 +0100

    Estructura Eclipse + Main con Lotería (inicial)

---
+++ src/edu/alumno/ramas/Main.java
@@ -0,0 +1,20 @@
+package edu.alumno.ramas;
+
+import java.util.HashSet;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        HashSet<Integer> bolas = new HashSet<>();
+        Random r = new Random();
+
+        while (bolas.size() < 5) {
+            bolas.add(r.nextInt(49) + 1);
+        }
+
+        System.out.println("Números principales: " + bolas);
+
+        int complementario = r.nextInt(49) + 1;
+        System.out.println("Complementario: " + complementario);
+    }
+}
